b4cca4486cb49c8e090b2de088e0d3e04d6ad7f5
root <root@LAPTOP-J0FUP5TH.localdomain>
2024-11-26 11:59:10 -0500

    update extractor download

--- dataset/prepare/download_extractor.sh
+++ dataset/prepare/download_extractor.sh
@@ -1,15 +1,11 @@
 rm -rf checkpoints
 mkdir checkpoints
 cd checkpoints
-echo -e "Downloading extractors"
-gdown --fuzzy https://drive.google.com/file/d/1o7RTDQcToJjTm9_mNWTyzvZvjTWpZfug/view
-gdown --fuzzy https://drive.google.com/file/d/1KNU8CsMAnxFrwopKBBkC8jEULGLPBHQp/view
+echo "Downloading"
+gdown "https://drive.google.com/uc?id=1AYsmEG8I3fAAoraT4vau0GnesWBWyeT8"
+echo "Extracting"
+tar xfzv t2m.tar.gz
+echo "Cleaning"
+rm t2m.tar.gz
 
-
-unzip t2m.zip
-unzip kit.zip
-
-echo -e "Cleaning\n"
-rm t2m.zip
-rm kit.zip
 echo -e "Downloading done!"
